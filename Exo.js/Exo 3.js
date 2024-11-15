c3cf704d34bfb4ef4343c4dc25aa13ac25f7c678
Loulrs <louloutelaurens@gmail.com>
2024-11-15 09:49:53 +0100

    4

---
+++ Exo.js/Exo 3.js
@@ -0,0 +1,15 @@
+//chaine de caractère
+var MavariableString = "toto";
+//nombre
+var MavariableNumerique = 100;
+//des tableaux
+var Mavariabletableau = ['Apple', 'Banana'];
+//des objets (ici la fenetre du navigateur) 
+var MavariableObjet = window
+
+var Mavariablehaut = window.innerHeight
+var Mavariabledesurveillance = window.innerWidth
+
+alert (MavariableString + MavariableNumerique + Mavariabletableau[0]+MavariableObjet.location.pathname+Mavariabledesurveillance);
+alert (Mavariablehaut)
+alert (Mavariabledesurveillance)
