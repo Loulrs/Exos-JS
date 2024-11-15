7ea75176523046964fb21ee5be3db4102e111922
Loulrs <louloutelaurens@gmail.com>
2024-11-15 10:32:59 +0100

    5

---
+++ Exo.js/Exo 4.js
@@ -0,0 +1,11 @@
+//chaine de caractère
+var MavariableString = "toto";
+//nombre
+var MavariableNumerique = prompt("entrez un nombre",100);
+
+if (MavariableString == "toto" && MavariableNumerique !=200 || MavariableNumerique <=100){
+    alert("je suis le cas 1");
+}else{
+    alert("je suis le cas 2");
+}
+//saisir le numéro 200
