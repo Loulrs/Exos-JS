43421052fe8febf3875059b66d0b27f629be42ca
Lou.lrs <louloutelaurens@gmail.com>
2024-11-23 15:25:02 +0100

    n

---
+++ Exo.js/Exo 8/Exo8.js
@@ -0,0 +1,7 @@
+var MaDiv1=document.getElementById("MaDiv1");
+MaDiv1.innerHTML="Hello";
+function ModifText(texte){
+alert("je vais modifier la div");
+MaDiv1.innerHTML=texte;
+}
+ModifText("Hello");
